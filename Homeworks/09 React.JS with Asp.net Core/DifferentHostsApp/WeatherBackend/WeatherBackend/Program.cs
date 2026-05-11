4c06763bd11328bd4a7c418df909da7a456b5017
ogurtsova <ogurtsova@AP-MVR3>
2026-05-11 14:08:41 +0300

    ASP.NET Core and React on the same and separate host

---
+++ Homeworks/09 React.JS with Asp.net Core/DifferentHostsApp/WeatherBackend/WeatherBackend/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyAllowSpecificOrigins",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("MyAllowSpecificOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
